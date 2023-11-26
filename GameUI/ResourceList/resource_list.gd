7da27242043656d37f6b3b247efa424016a0cf81
netfoley <netfoley@gmail.com>
2023-11-26 21:22:28 +0100

    Added shop animations

--- GameUI/ResourceList/resource_list.gd
+++ GameUI/ResourceList/resource_list.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export_dir var resources_path
 @onready var inventory_node : BaseUnit = %Inventory
+var shop_anim_timer = Timer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -34,6 +35,8 @@ func _ready():
 	GAME.res_bought.connect(_on_res_bought)
 	inventory_node.res_changed.connect(_on_inventory_changed)
 	inventory_node.changed.connect(_on_inventory_changed)
+	add_child(shop_anim_timer)
+	shop_anim_timer.timeout.connect(_on_shop_anim_timeout)
 	
 func _on_inventory_changed():
 	var comp : GameRes = inventory_node.parent_component
@@ -89,6 +92,36 @@ func _on_showbutton_toggled(toggle):
 		%ShowButton.text = ">"
 		
 func _on_res_bought(res : GameRes):
+	if shop_anim_timer.is_stopped():
+		shop_anim_timer.start(0.05)
+	for _i in pow(res.quantity, 0.3):
+		if icons_to_anim.size() < 100:
+			icons_to_anim.append(res.icon_text)
 	inventory_node.parent_component.add_component_at(0, res, true)
 	_on_inventory_changed()
 #	res.set_parent_game_res($ListContainer/TabContainer/Inventory.parent_component)
+
+func _on_shop_anim_timeout():
+	anim_text_to_inventory(icons_to_anim.pop_back())
+	shop_anim_timer.start(0.02 + randf() * 0.02)
+#	await get_tree().create_timer().timeout
+
+var icons_to_anim = []
+func anim_text_to_inventory(text : Texture2D):
+	if !is_instance_valid(text):
+		return
+	var t = create_tween()
+	var spr = TextureRect.new()
+	spr.texture = text
+	spr.top_level = true
+	spr.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	spr.size = Vector2(50,50)* (randf()*0.5 + 0.5)
+	add_child(spr)
+	var inv_rect = %TabContainer.get_tab_control(0).get_global_rect()
+	inv_rect.size.y -= %Shop.get_global_rect().size.y
+	var price_rect = %PriceCont.get_global_rect()
+	t.tween_property(spr, "global_position", inv_rect.position + inv_rect.size/2, 1).from(price_rect.position + price_rect.size * randf()).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	t.tween_callback(func(): spr.queue_free())
+	%TabContainer.get_child(0, true).pivot_offset = %TabContainer.get_child(0, true).size/2
+	t.tween_property(%TabContainer.get_child(0, true), "scale", Vector2(1.05,1.05), 0.03)
+	t.tween_property(%TabContainer.get_child(0, true), "scale", Vector2(1,1), 0.03)
--- Level/Camera2D.gd
+++ Level/Camera2D.gd
@@ -12,9 +12,6 @@ func _process(delta):
 	var minimum = min(float(get_viewport().get_visible_rect().size.x)/float(limit_right - limit_left), float(get_viewport().get_visible_rect().size.y)/float(limit_bottom - limit_top))
 	min_zoom = Vector2(minimum, minimum)
 	max_zoom = Vector2(maximum, maximum)
-	print(get_viewport().get_visible_rect().size.x)
-	print((limit_right - limit_left))
-	print(minimum)
 	target_zoom = Vector2(clamp(target_zoom.x, min_zoom.x, max_zoom.x), clamp(target_zoom.y, min_zoom.y, max_zoom.y))
 	zoom = lerp(zoom, target_zoom, 0.1)
 
